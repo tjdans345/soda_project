bfd70f944e0f65bfdcfa1011bc5c3261b4654ddb
meteor <tjdans34568@gmail.com>
2022-07-20 16:14:00 +0900

    테스트 코드 수정

--- pages/domain/main/chart/model/pay_object.js
+++ pages/domain/main/chart/model/pay_object.js
@@ -1,4 +1,6 @@
 export class PayObject {
+  test = 0; // ES 최신문법 테스트
+
   constructor(category, name, price, isA) {
     this.category = category;
     this.name = name;
@@ -9,5 +11,6 @@ export class PayObject {
   test() {
     console.log(this.category);
     console.log(this.price);
+    console.log(this.test);
   }
 }
